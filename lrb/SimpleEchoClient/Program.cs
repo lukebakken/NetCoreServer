8b3c3bc067a67b76c6bd5e86d6f0d549e56e417a
Luke Bakken <luke@bakken.io>
2024-02-08 16:01:25 -0800

    Use Pipelines

--- lrb/SimpleEchoClient/Program.cs
+++ lrb/SimpleEchoClient/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Buffers;
+using System.IO.Pipelines;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -8,28 +10,48 @@ static string Now()
     return DateTime.UtcNow.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
 }
 
+static void PrintBuffer(PipeReader reader, ReadResult result)
+{
+    ReadOnlySequence<byte> buffer = result.Buffer;
+    if (buffer.IsEmpty && result.IsCompleted)
+    {
+        Console.Error.WriteLine("[ERROR] {0} unexpected end!", Now());
+        Environment.Exit(1);
+    }
+
+    foreach (ReadOnlyMemory<byte> segment in buffer)
+    {
+        byte[] data = segment.ToArray();
+        Console.WriteLine("[INFO] {0} msg: {1}", Now(),
+            Encoding.ASCII.GetString(data));
+    }
+
+    reader.AdvanceTo(buffer.Start, buffer.End);
+}
+
 var ep = new IPEndPoint(IPAddress.Loopback, 1111);
 var client = new TcpClient(ep);
 await client.ConnectAsync(ep);
 NetworkStream ns = client.GetStream();
 
+var reader = PipeReader.Create(ns);
+var writer = PipeWriter.Create(ns);
 
 byte[] msg = Encoding.ASCII.GetBytes("HELLO!");
-await ns.WriteAsync(msg);
+await writer.WriteAsync(msg);
 
-byte[] buf = new byte[1024];
-int read = await ns.ReadAsync(buf);
-
-Console.WriteLine("[INFO] {0} read: {1}", Now(), read);
-Console.WriteLine("[INFO] {0} msg: {1}", Now(), Encoding.ASCII.GetString(buf));
+ReadResult result = await reader.ReadAsync();
+PrintBuffer(reader, result);
 
 using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1)))
 {
     try
     {
         Console.WriteLine("[INFO] {0} read with token start...", Now());
-        read = await ns.ReadAsync(buf, cts.Token);
-        Console.WriteLine("[INFO] {0} read with token end", Now());
+        result = await reader.ReadAsync(cts.Token);
+        PrintBuffer(reader, result);
+        Console.WriteLine("[INFO] {0} read with token end, result: {1}",
+            Now(), result);
     }
     catch (Exception ex)
     {
